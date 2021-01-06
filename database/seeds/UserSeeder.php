2ca3fd97240a58a71367d9758952429e013b9a69
Tariq Imtinan <timtinan@outlook.com>
2021-01-07 00:04:07 +0600

    make super admin

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\{
+	DB,
+	Hash
+};
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+        	'id' => 1,
+        	'name' => 'Super Admin',
+        	'email' => 'superAdmin@example.com',
+        	'email_verified_at' => now(),
+        	'password' => Hash::make('password'),
+        	'created_at' => now(),
+        	'updated_at' => now(),
+        ]);
+    }
+}
